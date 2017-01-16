4f63bd198ac6b8030212c25019b044ef1a831eaa
Daniel Johnson <danielj@ifixit.com>
2017-01-16 15:37:01 -0800

    clean up demo html page

--- src/demo.jsx
+++ src/demo.jsx
@@ -2,4 +2,6 @@ import WikiTableEditor from './wiki-table-editor.jsx'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
-ReactDOM.render((<WikiTableEditor />), document.querySelector('#root'));
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render((<WikiTableEditor />), document.querySelector('#root'));
+});
