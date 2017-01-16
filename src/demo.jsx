418fa02da330003c87ae1596b684a15a70880fa6
Daniel Johnson <danielj@ifixit.com>
2017-01-16 15:01:00 -0800

    Clean up webpack build
    * Rename 'table-test' to 'demo'.
    * Split the src files into 'wiki-table-editor.jsx' (which exports
      the component) and 'demo.jsx' for development/testing.
    * Add a command line argument for webpack for a dev or production build.

---
+++ src/demo.jsx
@@ -0,0 +1,5 @@
+import WikiTableEditor from './wiki-table-editor.jsx'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+ReactDOM.render((<WikiTableEditor />), document.querySelector('#root'));
